d3427029a75177ce95a60ac548ba41e10bea7aee
mahdi.hasanpor <mahdi.hasanpour2022@gmail.com>
2022-06-01 07:43:40 -0700

    navlink in navbar styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 // scss 
 import Styles from "../assets/styles/components styles/NavBar.module.scss";
 
@@ -6,7 +7,16 @@ const Navbar = () => {
   return (
     <>
       <div className={Styles.navbar} >
-
+        <nav>
+            <ul>
+              <li>
+                <NavLink className={(navdata) => navdata.isActive ? `${Styles.navlink} ${Styles.active}` : Styles.navlink} to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink className={navdata => navdata.isActive ? `${Styles.navlink} ${Styles.active}`: Styles.navlink} to="/products">Products</NavLink>
+              </li>
+            </ul>
+        </nav>
       </div>
     </>
   );
