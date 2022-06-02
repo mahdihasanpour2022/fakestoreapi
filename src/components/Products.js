6ef33763bbc5418910daeb45502d676941ddff6d
mahdi.hasanpor <mahdi.hasanpour2022@gmail.com>
2022-06-02 01:28:20 -0700

    filter and search category completed in products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 // action creator 
 import fetchData from '../redux/products/productsAC';
@@ -9,57 +9,70 @@ import Styles from "../assets/styles/components styles/Products.module.scss";
 
 const Products = () => {
 
+  const [filterCategory, setFilterCategory] = useState([])
   const [search, setSearch] = useState([])
-  const [categories, setCategories] = useState([])
   const dispatch = useDispatch();
   const { loading, products, error } = useSelector(state => state.productsState)
 
-
-  const findCategories = useCallback(() => {
+  const filterCategoryHandler = (e) => {
+    setFilterCategory(products.filter(item => item.category === e.target.name))
+    setSearch([])
+  }
+  const findCategories = () => {
     const c1 = products.map(item => item.category);
-    console.log(c1)
-
     const c2 = new Set(c1);
-    console.log(c2)
-    setCategories([...c2])
-  }, [products])
+    return [...c2]
+  }
 
   useEffect(() => {
     dispatch(fetchData());
-    findCategories()
-
-  }, [dispatch, findCategories])
+  }, [dispatch])
 
   const searchProduct = e => {
     setSearch(products.filter(item => item.category.includes(e.target.value)))
   }
-  console.log(categories)
+
+  console.log(filterCategory)
+  console.log(search)
+
   return (
     <>
+      {/* loading & error  */}
       <div className="container mt-2">
         <div className={`${Styles.loadingError} row `} >
           {!!loading && <p>loading products in progress ...</p>}
           {!!error && <p>{error}</p>}
         </div>
+        {/* search input  */}
         <div className="row mt-3">
           {!!products.length && <input onChange={e => searchProduct(e)} type="search" className={Styles.searchInput} placeholder='search...' />}
         </div>
+        {/* category filter  */}
         <div className="row">
           <div className={Styles.categories} >
-            {!!products.length && !!categories.length &&
-              categories.map(item =>
-                <div key={item.id} className={Styles.category} >
-                  <p>{item}</p>
+
+            {!!products.length &&
+              findCategories().map(item =>
+                <div key={item} className={Styles.category} >
+                  <button className={Styles.filterBTN} name={item} onClick={(e) => filterCategoryHandler(e)} >{item}</button>
                 </div>
               )
             }
+
           </div>
         </div>
+        {/* show product  */}
         <div className={`${Styles.products} row m-0 g-3`}>
           {!!products.length &&
+           
             !!search.length ?
-            search.map(item => <Product key={item.id} eachProductData={item} />) :
-            products.map(item => <Product key={item.id} eachProductData={item} />)}
+            search.map(item => <Product key={item.id} eachProductData={item} />)
+             :
+            !!filterCategory.length ?
+              filterCategory.map(item => <Product key={item.id} eachProductData={item} />)
+          :
+            products.map(item => <Product key={item.id} eachProductData={item} />)
+            }
         </div>
       </div>
     </>
